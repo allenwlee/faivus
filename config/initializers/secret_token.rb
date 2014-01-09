d9d58af82573c2c44c96a470c153be36b922a8b0
allenwlee <allenwlee@Allens-MacBook-Pro.local>
2014-01-08 23:34:48 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Faivus::Application.config.secret_key_base = 'fefdeb25bf49b59c133ae96074b72ab115d1129262cfd831ce060b3626a2f8d4bcbd9ea1f548823f22d36ba767164d8ffe336a909cd4f4db34ace6ae768c6c4d'
